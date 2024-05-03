8f096e2e20799eae5b821cd836fa2549455a5853
cealll <sealfajardo0706@gmail.com>
2024-05-03 20:50:50 +0800

    Fixed Login Screen (May 3, 2024)

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -1,22 +1,22 @@
 @section('title', 'Sign in to your account')
 
 <div>
-    <div class="sm:mx-auto sm:w-full sm:max-w-md">
-        <a href="{{ route('home') }}">
-            <x-logo class="w-auto h-16 mx-auto text-indigo-600" />
+    <div class="sm:mx-auto sm:w-full sm:max-w-md text-center flex flex-col items-center">
+        <a href="{{ route('dashboard') }}" class="inline-block">
+            <img src="{{ asset('storage/plmlogo/plm.png') }}" style="height: auto; max-height: 75px;" alt="PLM Logo">
         </a>
-
-        <h2 class="mt-6 text-3xl font-extrabold text-center text-gray-900 leading-9">
+    
+        <h2 class="mt-6 text-3xl font-semibold text-center text-blue-700 leading-9">
             Sign in to your account
         </h2>
-        @if (Route::has('register'))
+        {{-- @if (Route::has('register'))
             <p class="mt-2 text-sm text-center text-gray-600 leading-5 max-w">
                 Or
                 <a href="{{ route('register') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
                     create a new account
                 </a>
             </p>
-        @endif
+        @endif --}}
     </div>
 
     <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
@@ -52,14 +52,17 @@
 
                 <div class="flex items-center justify-between mt-6">
                     <div class="flex items-center">
-                        <input wire:model.lazy="remember" id="remember" type="checkbox" class="form-checkbox w-4 h-4 text-indigo-600 transition duration-150 ease-in-out" />
+                        <input wire:model.lazy="remember" id="remember" type="checkbox" class="form-checkbox w-4 h-4 text-blue-600 transition duration-150 ease-in-out" />
                         <label for="remember" class="block ml-2 text-sm text-gray-900 leading-5">
                             Remember
                         </label>
                     </div>
 
                     <div class="text-sm leading-5">
-                        <a href="{{ route('password.request') }}" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">
+                        {{-- <a href="{{ route('password.request') }}" class="font-medium text-blue-600 hover:text-blue-800 focus:outline-none focus:underline transition ease-in-out duration-150">
+                            Forgot your password?
+                        </a> --}}
+                        <a href="#" class="font-medium text-blue-600 hover:text-blue-800 focus:outline-none focus:underline transition ease-in-out duration-150">
                             Forgot your password?
                         </a>
                     </div>
@@ -67,7 +70,7 @@
 
                 <div class="mt-6">
                     <span class="block w-full rounded-md shadow-sm">
-                        <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
+                        <button type="submit" class="flex justify-center w-full px-4 py-2 text-sm font-medium text-white bg-blue-600 border border-transparent rounded-md hover:bg-blue-800 focus:outline-none focus:border-indigo-700 focus:ring-indigo active:bg-indigo-700 transition duration-150 ease-in-out">
                             Sign in
                         </button>
                     </span>
